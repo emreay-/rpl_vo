febbc0114d46c03f44f376e996359c409a4c3331
Emre Ay <emreay992@gmail.com>
2017-03-17 14:40:03 +0100

    Calculate Odometry function is getting called in the main now

--- rplvo_mono/src/mono.cpp
+++ rplvo_mono/src/mono.cpp
@@ -6,6 +6,16 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "mono_vo");
   std::string node_namespace = ros::this_node::getName();
   rplvo_mono::MonoOdometer mono(node_namespace);
-  ros::spin();
+//  ros::spin();
+  while (true) {
+    try {
+      ros::spinOnce();
+      mono.CalculateOdometry();
+    } catch (ros::Exception& e) {
+        ROS_WARN("Calculate Odometry exception: %s", e.what());
+        ros::Rate rate(1);
+        rate.sleep();
+    }
+  }
   return 0;
 }
