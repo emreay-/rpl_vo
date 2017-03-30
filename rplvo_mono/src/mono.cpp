cbc44e27896ee092d03b4ce9bf34acc74faf615d
Emre Ay <emreay992@gmail.com>
2017-03-30 18:38:04 +0200

    minor changes

--- rplvo_mono/src/mono.cpp
+++ rplvo_mono/src/mono.cpp
@@ -6,15 +6,14 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "mono_vo");
   std::string node_namespace = ros::this_node::getName();
   rplvo_mono::MonoOdometer mono(node_namespace);
-//  ros::spin();
+
   while (true) {
     try {
       ros::spinOnce();
       mono.CalculateOdometry();
     } catch (ros::Exception& e) {
-        ROS_WARN("Calculate Odometry exception: %s", e.what());
-        ros::Rate rate(1);
-        rate.sleep();
+      ROS_WARN("Calculate Odometry exception: %s", e.what());
+      ros::Duration(1).sleep();
     }
   }
   return 0;
