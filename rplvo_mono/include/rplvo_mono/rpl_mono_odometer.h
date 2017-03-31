c40bd18245c6c1ae1081d3ebab49a09caf84be5f
Emre Ay <emreay992@gmail.com>
2017-03-31 15:29:47 +0200

    namespace changed to a more logical structure, visualization parameters and members removed and migrated to its header, new member visualizer object pointer, creating and hence deleting visualizer object based on bool parameter

--- rplvo_mono/include/rplvo_mono/rpl_mono_odometer.h
+++ rplvo_mono/include/rplvo_mono/rpl_mono_odometer.h
@@ -2,6 +2,8 @@
 #define rpl_mono_odometer_h
 
 #include "rplvo_core/odometer_base.h"
+#include "rplvo_core/rplvo_core.h"
+#include "rplvo_core/rplvo_visualizer.h"
 #include <vector>
 #include <string>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -12,95 +14,79 @@
 #include <vikit/params_helper.h>
 #include <vikit/abstract_camera.h>
 
-namespace rplvo_mono {
-
-  typedef std::vector< cv::KeyPoint > KeyPointVector;
-  typedef std::vector< cv::Point2f > PointVector;
-  typedef std::vector < PointVector > PointContainer;
-  typedef std::vector < cv::Scalar > ScalarVector;
-
-  ///
-  /// \struct MonoOdometerParameters
-  /// \brief The MonoOdometerParameters struct which contains the parameters needed to run the odometer for feature detection, feature tracking, RANSAC and so on.
-  ///
-  struct MonoOdometerParameters {
-    int min_number_of_features;
-    double ransac_threshold;
-    double ransac_confidence;
-    std::string image_topic;
-    bool rectify_image;
-    int feature_detector_threshold;
-    int feature_tracker_window_size;
-    int feature_tracker_max_pyramid_level;
-    int feature_tracker_max_iterations;
-    double feature_tracker_epsilon;
-    double feature_tracker_eigen_threshold;
-    bool visualize;
-    int visualize_frame_tracking;
-    int visualize_color_variety;
-
+namespace rplvo {
+  namespace mono {
 
     ///
-    /// \fn Read
-    /// \brief Gathers all the values for members from ROS parameter server. Assings deafult values in case of failure.
-    /// \param node_namespace String to state the namespace of the node which all the parameters would be gathered relatively.
+    /// \struct MonoOdometerParameters
+    /// \brief The MonoOdometerParameters struct which contains the parameters needed to run the odometer for feature detection, feature tracking, RANSAC and so on.
     ///
-    void Read(const std::string node_namespace) {
-      min_number_of_features = vk::getParam<int>(node_namespace+"/min_number_of_features",100);
-      ransac_threshold = vk::getParam<double>(node_namespace+"/ransac_threshold",1.0);
-      ransac_confidence = vk::getParam<double>(node_namespace+"/ransac_confidence",0.99);
-      image_topic = vk::getParam<std::string>(node_namespace+"/image_topic","/camera/image_raw");
-      rectify_image = vk::getParam<bool>(node_namespace+"/rectify_image",true);
-      feature_detector_threshold = vk::getParam<int>(node_namespace+"/feature_detector_threshold",40);
-      feature_tracker_window_size = vk::getParam<int>(node_namespace+"/feature_tracker_window_size",21);
-      feature_tracker_max_pyramid_level = vk::getParam<int>(node_namespace+"/feature_tracker_max_pyramid_level",2);
-      feature_tracker_max_iterations = vk::getParam<int>(node_namespace+"/feature_tracker_max_iterations",30);
-      feature_tracker_epsilon = vk::getParam<double>(node_namespace+"/feature_tracker_epsilon",0.01);
-      feature_tracker_eigen_threshold = vk::getParam<double>(node_namespace+"/feature_tracker_eigen_threshold",0.0001);
-      visualize = vk::getParam<bool>(node_namespace+"/visualize",false);
-      visualize_frame_tracking = vk::getParam<int>(node_namespace+"/visualize_frame_tracking",10);
-      visualize_color_variety = vk::getParam<int>(node_namespace+"/visualize_color_variety",50);
-    }
-  }; /* struct MonoOdometerParameters */
+    struct MonoOdometerParameters {
+      int min_number_of_features;
+      double ransac_threshold;
+      double ransac_confidence;
+      std::string image_topic;
+      bool rectify_image;
+      int feature_detector_threshold;
+      int feature_tracker_window_size;
+      int feature_tracker_max_pyramid_level;
+      int feature_tracker_max_iterations;
+      double feature_tracker_epsilon;
+      double feature_tracker_eigen_threshold;
+      bool visualize;
 
-  ///
-  /// \class MonoOdometer
-  /// \brief Class to implement monocular visual odometry
-  ///
-  class MonoOdometer : public viso2_ros::OdometerBase {
-  public:
-    MonoOdometer(std::string node_namespace);
-    ~MonoOdometer();
-    vk::AbstractCamera* camera_ptr_;
-    void CalculateOdometry();
+      ///
+      /// \fn Read
+      /// \brief Gathers all the values for members from ROS parameter server. Assings deafult values in case of failure.
+      /// \param node_namespace String to state the namespace of the node which all the parameters would be gathered relatively.
+      ///
+      void Read(const std::string node_namespace) {
+        min_number_of_features = vk::getParam<int>(node_namespace+"/min_number_of_features",100);
+        ransac_threshold = vk::getParam<double>(node_namespace+"/ransac_threshold",1.0);
+        ransac_confidence = vk::getParam<double>(node_namespace+"/ransac_confidence",0.99);
+        image_topic = vk::getParam<std::string>(node_namespace+"/image_topic","/camera/image_raw");
+        rectify_image = vk::getParam<bool>(node_namespace+"/rectify_image",true);
+        feature_detector_threshold = vk::getParam<int>(node_namespace+"/feature_detector_threshold",40);
+        feature_tracker_window_size = vk::getParam<int>(node_namespace+"/feature_tracker_window_size",21);
+        feature_tracker_max_pyramid_level = vk::getParam<int>(node_namespace+"/feature_tracker_max_pyramid_level",2);
+        feature_tracker_max_iterations = vk::getParam<int>(node_namespace+"/feature_tracker_max_iterations",30);
+        feature_tracker_epsilon = vk::getParam<double>(node_namespace+"/feature_tracker_epsilon",0.01);
+        feature_tracker_eigen_threshold = vk::getParam<double>(node_namespace+"/feature_tracker_eigen_threshold",0.0001);
+        visualize = vk::getParam<bool>(node_namespace+"/visualize",false);
+      }
+    }; // struct MonoOdometerParameters
 
-  protected:
-    std::string node_namespace_;
-    MonoOdometerParameters parameters_;
-    PointVector DetectFeatures(cv::Mat input_image, cv::Mat output_image);
-    void TrackFeatures();
-    void EstimateMotion();
-    void Visualize();
+    ///
+    /// \class MonoOdometer
+    /// \brief Class to implement monocular visual odometry
+    ///
+    class MonoOdometer : public viso2_ros::OdometerBase {
+    public:
+      MonoOdometer(std::string node_namespace);
+      ~MonoOdometer();
+      vk::AbstractCamera* camera_ptr_;
+      void CalculateOdometry();
 
-  private:
-    void ImageCallback(const sensor_msgs::ImageConstPtr& msg);
-    void DrawCircles(cv::Mat image, PointContainer feature_container);
-    void DrawLines(cv::Mat image, PointContainer feature_container);
-    ScalarVector color_vector_;
-    image_transport::Subscriber image_sub_;
-    ros::Publisher image_pub_;
-    ros::Publisher temp_pub_;
-    cv::Mat current_image_;
-    cv::Mat previous_image_;
-    cv::Mat image_to_draw_features_;
-    PointVector previous_features_;
-    PointVector current_features_;
-    int draw_accumulate_;
-    cv::Mat sketch_;
-    PointContainer feature_container_;
-    bool first_run_;
+    protected:
+      std::string node_namespace_;
+      MonoOdometerParameters parameters_;
+      PointVector DetectFeatures(cv::Mat input_image);
+      void TrackFeatures();
+      void EstimateMotion();
+      void Visualize();
+      visualizer::Visualizer* visualizer_ptr_;
 
-  }; /* class MonoOdometer */
-} /* namespace rplvo_mono */
+    private:
+      void ImageCallback(const sensor_msgs::ImageConstPtr& msg);
+      image_transport::Subscriber image_sub_;
+      ros::Publisher image_pub_;
+      cv::Mat current_image_;
+      cv::Mat previous_image_;
+      PointVector previous_features_;
+      PointVector current_features_;
+      bool first_run_;
 
+    }; // class MonoOdometer
+  } // namespace mono
+}// namespace rplvo
 #endif
